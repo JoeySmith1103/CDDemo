2c89842d12bc0a412e77dc45f085ef66288d56a7
JoeySmith1103 <stu30405@gmail.com>
2024-06-04 10:19:47 +0800

    test7

--- src/main/java/com/example/cddemo/CdDemoApplication.java
+++ src/main/java/com/example/cddemo/CdDemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.cddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class CdDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CdDemoApplication.class, args);
 	}
 
 	@GetMapping("/")
